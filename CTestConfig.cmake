22cff0781ca7e62e75c0b1aba7fa2a9061cacd3a
Carlos-Luque <carlos.luque@ulpgc.es>
2019-09-03 15:47:49 +0100

    Fix: set(CDASH_PROJECT_NAME "SlicerTranslated")

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,6 @@
 set(CTEST_PROJECT_NAME "Sliceri18nl10n")
 set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
-
-if(NOT DEFINED CDASH_PROJECT_NAME)
-  set(CDASH_PROJECT_NAME "SlicerTranslated")
-endif()
+set(CDASH_PROJECT_NAME "SlicerTranslated")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "jerry.iuibs.ulpgc.es")
